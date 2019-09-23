0e9f813913d2c23b228e41023ebe813150eab745
Franklin3143 <43715103+Franklin3143@users.noreply.github.com>
2019-09-23 09:58:44 -0300

    Commit Inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  public jogoEmAndamento: boolean = true;
+
+  public encerrarJogo(tipo: string): void {
+    console.log(tipo);
+    this.jogoEmAndamento = false;
+  }
+}
---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css']
+})
+export class PainelComponent implements OnInit, OnDestroy {
+
+  public frases: Frase[] = FRASES;
+  public instrucao: string = 'Traduza a frase:';
+  public resposta: string = '';
+
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  public progresso: number = 0;
+
+  public tentativas: number = 3;
+
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter();
+
+  constructor() {
+    this.atualizaRodada();
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    console.log('Componente painel foi destruído!')
+  }
+
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+  }
+
+  public verificarResposta(): void {
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+
+      //trocar pergunta da rodada
+      this.rodada++
+
+      //Progresso
+      this.progresso = this.progresso + (100 / this.frases.length)
+
+      //
+      if(this.rodada === 4) {
+        this.encerrarJogo.emit('Vitória!!!')
+      }
+
+      //atualiza o objeto rodadaFrase
+      this.atualizaRodada();
+
+      //Limpar resposta
+      this.resposta = '';
+
+    } else {
+      this.tentativas--
+
+      if (this.tentativas === -1) {
+        this.encerrarJogo.emit('Derrota!!!')
+      }
+    }
+
+  }
+
+  atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada]
+  }
+
+}
